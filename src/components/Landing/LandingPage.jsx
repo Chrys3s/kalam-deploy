0f7ae33a5e90db064cf47f3303fb8c9cab6449c2
Gaurav Goyal <gaurav.g@safe.security>
2021-10-24 16:40:11 +0530

    90% home page complete

--- src/components/Landing/LandingPage.jsx
+++ src/components/Landing/LandingPage.jsx
@@ -1,64 +1,102 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Footer from '../Footer/Footer';
-import CardEffect from '../CardEffect/CardEffect';
-import bookGif from '../../assets/images/openBook.gif';
-import './LandingPage.css';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Footer from "../Footer/Footer";
+import BlogImage from "../../assets/images/blog-img.jpeg";
+import PracticeGif from "../../assets/images/code.gif";
+import stroke from "../../assets/images/stroke.png";
+import utilitySlice from "../../slices/utilitySlice";
+import "./LandingPage.css";
 
 const LandingPage = () => {
-	useEffect(() => {
-		document.title = 'कलम 🖋';
-	}, []);
+  const comingForAbout = useSelector(
+    (state) => state.utilitySlice.comingForAbout
+  );
+  const dispatch = useDispatch();
 
-	return (
-		<main>
-			<main className="home-bg h-screen -mt-14 relative">
-				<main className="description text-white">
-					<span className="mt-14 absolute text-6xl font-medium top-20 left-32 main-text z-30">
-						कलम
-					</span>
-					<p className="mt-14 absolute text-3xl md:text-xl font-normal top-40 left-32">
-						An Interactive Educational Platform
-					</p>
-					<p className="mt-14 absolute text-3xl md:text-xl font-normal top-48 left-32">
-						For Web Development
-					</p>
-				</main>
-			</main>
-			<CardEffect />
-			{/* <main className="landing-2-img">
-				<main className="flex justify-center items-center det-1 relative h-96">
-					<h1 className="absolute">Blogs</h1>
-				</main>
-				<main className="flex justify-end det-2">
-					<main className="det-3 relative">
-						<h1>Practice</h1>
-					</main>
-				</main>
-				<main className="flex justify-center items-center det-1 relative h-96">
-					<h1 className="absolute">Blogs</h1>
-				</main> 
-			</main> */}
-			{/* <main className="flex flex-row justify-evenly items-center text-black mt-20 mb-10 text-2xl">
-				<main className="bg-yellow-200 some-round-shape flex justify-center items-center hover:underline hover:animate-bounce">
-					<Link to="/blogs">
-						<span className="text-rotate-custom">Blogs</span>
-					</Link>
-				</main>
-				<main className="bg-yellow-200 some-round-shape flex justify-center items-center hover:underline hover:animate-bounce">
-					<Link to="/blogs">
-						<span className="text-rotate-custom">Practice</span>
-					</Link>
-				</main>
-				<main className="bg-yellow-200 some-round-shape flex justify-center items-center hover:underline hover:animate-bounce">
-					<Link to="/blogs">
-						<span className="text-rotate-custom">About</span>
-					</Link>
-				</main>
-			</main> */}
-			<Footer />
-		</main>
-	);
+  useEffect(() => {
+    document.title = "कलम 🖋";
+    if (comingForAbout) {
+      window.scroll(
+        0,
+        window.scrollY +
+          document.getElementById("about-us-section").getBoundingClientRect()
+            .top
+      );
+      dispatch(utilitySlice.actions.setComingForAbout(false));
+      return;
+    }
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <main>
+      <main className="home-bg h-screen -mt-14 relative">
+        <main className="description text-white">
+          <span className="mt-14 absolute text-6xl font-medium top-20 left-32 main-text z-30">
+            कलम
+          </span>
+          <p className="mt-14 absolute text-3xl md:text-xl font-normal top-40 left-32">
+            An Interactive Educational Platform
+          </p>
+          <p className="mt-14 absolute text-3xl md:text-xl font-normal top-48 left-32">
+            For Web Development
+          </p>
+        </main>
+      </main>
+      <main className="landing-2-img p-4">
+        <main className="flex justify-start mb-4">
+          <img src={BlogImage} className="blogImage" />
+          <p className="m-2 flex justify-center items-center text-justify w-2/3 pl-10 section-font">
+            Blogs are meant to provide learning resources for learning the
+            technology that you wish to work with. These blogs are categorised
+            in multiple tags which empowers direct navigation to required
+            content. Blogs are free for anyone to read but only administrators
+            can publish them.
+          </p>
+        </main>
+        <br />
+        <hr />
+        <br />
+        <main className="flex justify-end">
+          <p className="m-2 flex justify-center items-center text-justify w-2/3 pr-10 section-font">
+            Practise empowers the hands-on experience of writing the code and
+            getting instant outputs. It will no longer be just receding blogs or
+            getting theoretical knowledge but to actually apply the concepts
+            learned right there and then.
+          </p>
+          <img src={PracticeGif} className="codeGif" />
+        </main>
+        <br />
+        <hr />
+        <br />
+        <main className="mb-4 about-us-bg" id="about-us-section">
+          <main className="flex justify-center items-center section-font-heading my-4">
+            About Us
+          </main>
+          <img
+            src={stroke}
+            className="w-96 h-10 block ml-auto mr-auto -mt-8 pl-16 mb-12"
+          />
+          <p className="section-font-heading-p px-10 text-justify">
+            Kalam is the interactive multi-facet learning platform especially
+            for web development. We are a team of 4 computer science engineering
+            students (Ashish, Asmit, Gargeya, Gaurav) from University of
+            Petroleum and Energy Studies pursuing the course specialised in
+            Cyber Security and Forensics. We thought of Kalam as our small
+            contribution to society which is urging to seek digital education
+            from and throughout the world. Due to recent pandemic, we all were
+            forced to change our lifestyle and make even the most mundane tasks
+            virtual and digital. Due to such a pressure, it was a time of crisis
+            of numerous sectors but especially education sector. Kalam provides
+            a way to do all the basic tasks required to learn web development at
+            one place. You can learn the concepts through blogs and then can
+            perform hands on practise on the practise feature.
+          </p>
+        </main>
+      </main>
+      <Footer />
+    </main>
+  );
 };
 
 export default LandingPage;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,82 +1,96 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import utilitySlice from '../../slices/utilitySlice';
-import switchSound from '../../assets/audio/switch-8.mp3';
-import { FaSun } from 'react-icons/fa';
-import { FaMoon } from 'react-icons/fa';
-import { FaSignInAlt } from 'react-icons/fa';
-import DropDown from '../DropDown/DropDown';
+import React, { useState } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import utilitySlice from "../../slices/utilitySlice";
+import switchSound from "../../assets/audio/switch-8.mp3";
+import { FaSun } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
+import { FaSignInAlt } from "react-icons/fa";
+import DropDown from "../DropDown/DropDown";
 
-const Navbar = props => {
-	const dispatch = useDispatch();
-	const { userInfo } = useSelector(state => state.userInfo);
-	const { onClick, val } = props;
-	const [showDropDown, setShowDropDown] = useState(false);
+const Navbar = (props) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+  const { userInfo } = useSelector((state) => state.userInfo);
+  const { onClick, val } = props;
+  const [showDropDown, setShowDropDown] = useState(false);
 
-	const showPopup = () => {
-		dispatch(utilitySlice.actions.displayPopup(true));
-	};
+  const showPopup = () => {
+    dispatch(utilitySlice.actions.displayPopup(true));
+  };
 
-	const switchMode = async () => {
-		onClick(!val);
-		await new Audio(switchSound).play();
-	};
+  const switchMode = async () => {
+    onClick(!val);
+    await new Audio(switchSound).play();
+  };
 
-	return (
-		<main className="sticky top-0 z-10 flex justify-between bg-customN-light dark:bg-customN-dark dark:text-white h-14 shadow-xl dark:shadow-2xl opacity-95">
-			<main className="flex justify-center items-center">
-				<span className="p-2 m-1 mx-3 text-3xl font-semibold">
-					<Link to="/">क</Link>
-				</span>
-			</main>
-			<main className="flex">
-				<button
-					className="p-2 m-1 curson-not-allowed hover:underline"
-					disabled
-				>
-					{/* <Link to="/blogs">Blogs</Link> */}
-					Blogs
-				</button>
-				<button className="p-2 m-1 mx-12 hover:underline">
-					<Link to="/practice">Practice</Link>
-				</button>
-				<button
-					className="p-2 m-1 cursor-not-allowed hover:underline"
-					disabled
-				>
-					{/* <Link to="/about">About</Link> */}
-					About
-				</button>
-			</main>
-			<main className="flex justify-around h-14 items-center mr-4">
-				<span
-					className="cursor-pointer p-2 m-1 mr-2 text-2xl"
-					onClick={switchMode}
-				>
-					{val && <FaSun color="fff200" />}
-					{!val && <FaMoon />}
-				</span>
-				{!userInfo.isLoggedIn ? (
-					<button onClick={showPopup} className="p-2 m-1 text-2xl">
-						<FaSignInAlt />
-					</button>
-				) : (
-					<>
-						<button
-							className="rounded-2xl bg-red-500 h-8 w-8 items-center text-center border-none"
-							onClick={() => {
-								setShowDropDown(!showDropDown);
-							}}
-						>
-							{userInfo.userName[0].toUpperCase()}
-						</button>
-						<DropDown show={showDropDown} />
-					</>
-				)}
-			</main>
-		</main>
-	);
+  const goToAboutSection = () => {
+    const aboutUs = document.getElementById("about-us-section");
+    if (aboutUs) {
+      window.scroll(0, window.scrollY + aboutUs.getBoundingClientRect().top);
+      return;
+    }
+    dispatch(utilitySlice.actions.setComingForAbout(true));
+    history.push("/");
+  };
+
+  const refreshState = () => {
+    if (location.pathname !== "/") {
+      history.push("/");
+    }
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <main className="sticky top-0 z-10 flex justify-between bg-customN-light dark:bg-customN-dark dark:text-white h-14 shadow-xl dark:shadow-2xl opacity-95">
+      <main className="flex justify-center items-center">
+        <span
+          className="p-2 m-1 mx-3 text-3xl font-semibold cursor-pointer"
+          onClick={refreshState}
+        >
+          क
+        </span>
+      </main>
+      <main className="flex">
+        <button className="p-2 m-1 curson-not-allowed hover:underline" disabled>
+          <Link to="/blogs">Blogs</Link>
+        </button>
+        <button className="p-2 m-1 mx-12 hover:underline">
+          <Link to="/practice">Practice</Link>
+        </button>
+        <button className="p-2 m-1 hover:underline" onClick={goToAboutSection}>
+          About
+        </button>
+      </main>
+      <main className="flex justify-around h-14 items-center mr-4">
+        <span
+          className="cursor-pointer p-2 m-1 mr-2 text-2xl"
+          onClick={switchMode}
+        >
+          {val && <FaSun color="fff200" />}
+          {!val && <FaMoon />}
+        </span>
+        {!userInfo.isLoggedIn ? (
+          <button onClick={showPopup} className="p-2 m-1 text-2xl">
+            <FaSignInAlt />
+          </button>
+        ) : (
+          <>
+            <button
+              className="rounded-2xl bg-red-500 h-8 w-8 items-center text-center border-none"
+              onClick={() => {
+                setShowDropDown(!showDropDown);
+              }}
+            >
+              {userInfo.userName[0].toUpperCase()}
+            </button>
+            <DropDown show={showDropDown} />
+          </>
+        )}
+      </main>
+    </main>
+  );
 };
 
 export default Navbar;
